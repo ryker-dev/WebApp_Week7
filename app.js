514ae35d939adff2151ca800a4dfd9761d2dd13d
ryker-dev <pizzuti.dev@gmail.com>
2022-07-27 22:56:35 +0300

    Fixed register

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/user');
+const apiUserRouter = require('./api/user');
 
 const app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
+app.use('/api/user', apiUserRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
